9d6e58510fde6d31257177a3b61ef34509be2869
leolarrel <leolarrel@localhost>
2012-11-22 02:46:04 +0800

    update read command logic

--- order_server/command.c
+++ order_server/command.c
@@ -1,71 +1,29 @@
 /*
     command list
-
     1:login
     2:set account
-    3:order
-    4:logout
-    5:exit
+    3:logout
+    4:exit
+    5:commit market order & push directly
+    6:commit price order
+    7:push all order
 
     command report
     a.login report
     b.order report
     c.account list report
 
-    encode
-    1:login
-    char:size|char:command:=1|Cstr:loginID|Cstr:password
-
-    2:set account
-    char:size|char:command:=2|integer:set
-
-    3:order
-    4:logout
-    char:size|char:command:=4
-
-    5:exit
-    char:size|char:command:=5
 */
 
 
-extern char  RunCommand(void* datab,int size);
-char  RunCommand(void* datab,int size) {
+char  RunCommand(int __cs);
+char  RunCommand(int __cs) {
     /*
-    if state is start
-        get command length
-    
-        if size < command legth
-        then
-            not full
-            copy data into command buffer
-            state become wait left
-        else
-            full 
-            copy into command buffer
-            run command
-            if size = legth
-                clear command buffer
-            else 
-                recall myself and use rest data
-            endif
-        endif
-        
-    else state is wait
-        if size < rest command legth
-        then
-            still not full ! shit
-            copy data into command buffer
-            state still wait left
-        else
-            full
-            copy into command buffer
-            run command
-            if size = legth left
-                clear command buffer
-            else 
-                recall myself and use rest data
-            endif
-    endif
+    recv to get one byte (size)
+    recv to get data of size bytes into command buffer
+    now , I get full command data
+    run parse;
+    loop
     */
 }
 
