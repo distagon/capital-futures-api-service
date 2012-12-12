02ed1180b8436af6b2edfb161cb5ce1225b5abed
leolarrel <leolarrel@localhost>
2012-12-12 20:45:44 +0800

    quote_server : implement command code

--- quote_server/command.c
+++ quote_server/command.c
@@ -1,4 +1,145 @@
 #include <windows.h>
 #include <stdio.h>
 #include <winsock2.h>
+#include "quoteapi.h"
 
+/*command
+
+login
+struct Login {
+    char command:=2;
+}
+
+logout
+struct Logout {
+    char command:=3;
+}
+
+exit
+struct Exit {
+    char command:=4;
+}
+
+watch Commodity name
+struct Watch {
+    char command:=5;
+    char watch[10];
+}
+
+pull price
+struct Pull {
+    char command:=6;
+    int  index; -> little_endian
+}
+*/
+
+/*logic
+
+server(this)                                client
+        <-----------------------------------Login
+report login result >>>>>>>>>>>>>>>>>>>>>>>>
+
+        <-----------------------------------Watch
+report watch result >>>>>>>>>>>>>>>>>>>>>>>>
+
+        <-----------------------------------Pull
+report price >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+        <-----------------------------------Pull
+report price >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        <-----------------------------------Pull
+report price >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+.
+.
+.
+.
+.
+.
+.
+        <-----------------------------------Logout
+NULL report
+
+        <-----------------------------------Exit
+Server exit
+*/
+
+
+
+#define _Start      1
+#define _FILLING    2
+
+static int  _state = _Start;
+static int  _cmdsize = 0;
+static int  _fillcur = 0;
+static char cmdbuf[64];
+
+extern char LoginID[16]; //define in main.c
+extern char Password[16];//define in main.c
+
+static void __stdcall ConnectN ( int nKind, int nCode );
+void        __stdcall ConnectN ( int nKind, int nCode ) {
+    printf("Connect callback notify. nKind = %d , nCode = %d\n",nKind,nCode);
+}
+
+
+static void __stdcall TickN    ( short sMarketNo, short sStockidx, int nPtr);
+void        __stdcall TickN    ( short sMarketNo, short sStockidx, int nPtr){
+    printf("Tick callback notify. sMarketNo = %d,sStockidx = %d,nPtr = %d\n",sMarketNo,sStockidx,nPtr);
+}
+
+
+
+static int WhatCommand(void);
+static int WhatCommand(void) {
+    switch (cmdbuf[0]) {
+        case 1:
+        case 2:
+            printf("bp1\n");
+            QL_LoginServer(LoginID,Password);
+            QL_AddCallBack((long)ConnectN,(long)TickN);
+            QL_ConnectDataBase();
+            break;
+
+        case 3:
+            QL_Bye();
+            break;
+
+        case 4:
+            return 0;
+            
+        case 5:
+            printf("bp2,watch is %s\n",cmdbuf+1);
+            QL_Request(cmdbuf+1);
+            break;
+
+        case 6:
+            break;
+    }
+    
+    return 1;
+}
+
+extern int RunCommand(char data);
+int RunCommand(char data) {
+
+    if( _state == _Start) {
+        _cmdsize = data;
+        _fillcur = 0;
+        _state = _FILLING;
+    }
+
+    else if (_state == _FILLING) {
+        cmdbuf[_fillcur] = data;
+        _fillcur ++;
+        if( _fillcur == _cmdsize) {
+            /*parse command*/
+            if( WhatCommand() == 0 ) return 0;
+            _state = _Start;
+        }
+    }
+
+    return 1;
+}
--- quote_server/main.c
+++ quote_server/main.c
@@ -1,7 +1,6 @@
 #include <windows.h> 
 #include <winsock2.h> 
 #include <stdio.h>
-#include "quoteapi.h"
 
 const char      g_szClassName[] = "quoteview";
 unsigned int    _Tid;
@@ -9,11 +8,15 @@ HANDLE          __ref_get_data_thread = NULL;
 int             __last_trade_id = 0;
 short int       __market,__stock;
 int             __client_s=-1;
-char            LoginID[16];
-char            Password[16];
 int             DefaultPort=3396;
 #define WM_SOCKET WM_USER+101
 
+extern int RunCommand(char data); //define in command.c
+
+extern char LoginID[16];
+extern char Password[16];
+char LoginID[16];
+char Password[16];
 
 
 
@@ -47,65 +50,23 @@ int __parse_argv(void) {
     return 1;
 }
 
-DWORD WINAPI __get_tick_thread(LPVOID lpArg);
-DWORD WINAPI __get_tick_thread(LPVOID lpArg) {
-    TTick   data;
-    int __local_nptr = 0;
-    char buffer[16];
 
-    while(1) {
-        while(__local_nptr < __last_trade_id) {
-            QL_GetTick(__market,__stock,__local_nptr,&data);
-            //printf("Get data. tick price is %d\n",data.m_nClose);
-            sprintf(buffer,"%d\n",data.m_nClose);
-            send(__client_s,buffer,strlen(buffer),0);
-            __local_nptr++;
-        }
-        SuspendThread(__ref_get_data_thread);
-    }
-    return 0;
-}
-
-void CALLBACK TimerProc(HWND hwnd, UINT message, UINT timerID, DWORD time) ;
-void CALLBACK TimerProc(HWND hwnd, UINT message, UINT timerID, DWORD time) {
-    QL_Request("""");//this line will compile fail. that is tip
-    KillTimer(NULL,_Tid);
-}
-
-
-void    __stdcall ConnectN ( int nKind, int nCode );
-void    __stdcall ConnectN ( int nKind, int nCode ) {
-    printf("Connect callback notify. nKind = %d , nCode = %d\n",nKind,nCode);
-    _Tid = SetTimer(NULL,0,1000,TimerProc);
-    __ref_get_data_thread = CreateThread( NULL, 0,__get_tick_thread, NULL,CREATE_SUSPENDED, NULL);
-}
-
-
-void    __stdcall TickN    ( short sMarketNo, short sStockidx, int nPtr);
-void    __stdcall TickN    ( short sMarketNo, short sStockidx, int nPtr){
-    printf("Tick callback notify. sMarketNo = %d,sStockidx = %d,nPtr = %d\n",sMarketNo,sStockidx,nPtr);
-    __last_trade_id = nPtr;
-    __market    = sMarketNo;
-    __stock     = sStockidx;
-    ResumeThread(__ref_get_data_thread);
-}
 
 
 void SocketHaveData(HWND hwnd,int _socket,LPARAM _l);
 void SocketHaveData(HWND hwnd,int _socket,LPARAM _l) {
 
-    char buffer[256];
+    char buffer;
     int bi;
 
 
-    if (WSAGETSELECTERROR(_l)) 
-    { 
+    if (WSAGETSELECTERROR(_l)) {
         closesocket(_socket);
         return;
-    } 
+    }
 
-    switch (WSAGETSELECTEVENT(_l)) 
-    { 
+    switch (WSAGETSELECTEVENT(_l))  {
+
         case FD_ACCEPT:
             if(__client_s == -1) {
                 __client_s=accept(_socket,NULL,NULL);
@@ -116,31 +77,23 @@ void SocketHaveData(HWND hwnd,int _socket,LPARAM _l) {
             }
             break;
 
+
+
         case FD_READ:
-            bi=recv(_socket,buffer,256,0);
-            if ( (bi==SOCKET_ERROR&& WSAGetLastError() == WSAECONNRESET) || bi==0 )
+            bi=recv(_socket,&buffer,1,0);
+            if ( bi==SOCKET_ERROR || bi==0 )
             {
                 printf("recv() fail\n");
                 closesocket(_socket);
-            }
-            else if (bi>0)
-            {
-                if(buffer[0] == '1') {
-                    __ref_get_data_thread = NULL;
-                    QL_LoginServer(LoginID,Password);
-                    QL_AddCallBack((long)ConnectN,(long)TickN);
-                    QL_ConnectDataBase();
-                }
-                else if (buffer[0] == '2') {
-                    printf("kill Capital API\n");
-                    QL_Bye();
-                }
-                else if(buffer[0] == '3') {
-                    printf("Call DestoryWindow()\n");
-                    DestroyWindow(hwnd);
-                }
+            } else bi = RunCommand(buffer);
+            
+            if(bi == 0) {
+                closesocket(_socket);
+                DestroyWindow(hwnd);
             }
             break;
+
+
 
         case FD_CLOSE:
             closesocket(_socket);
@@ -244,7 +197,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 
 
 
-    ShowWindow(hwnd,SW_SHOWNORMAL);
+    ShowWindow(hwnd,SW_SHOW);
     UpdateWindow(hwnd);
     while (GetMessage(&Msg,0,0,0)) 
     { 
