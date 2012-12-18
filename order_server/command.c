b0ba049e7696de621b3daf15f0d3c7c23fa9a399
leolarrel <leolarrel@localhost>
2012-12-18 20:19:04 +0800

    order_server : main.c & command.c , output message standardization

--- order_server/command.c
+++ order_server/command.c
@@ -3,8 +3,18 @@
 #include <stdio.h>
 #include "orderapi.h"
 
+#ifdef DEBUG
+#define _D(x)	printf x
+#else
+#define _D(x)
+#endif
+
+#define	_L(x)	printf x
+
+
 extern char LoginId[32];    //declare on main.c,LoginID
 extern char Password[128];  //declare on main.c,Login Password
+
 /*
    command list
 	1:login
@@ -44,6 +54,73 @@ command encode:
 	char:size|char:command->7
 
  */
+
+char __login(void);
+char __logout(void);
+char __account(char index);
+char __push_market_order(char* cmdbuf);
+char __commit_order(char* cmdbuf);
+char __push_order(char* cmdbuf);
+
+char  __run_command_parse(int socket,void* cmd,int cmdsize);
+char  __run_command_parse(int socket,void* cmd,int cmdsize)
+{
+	char* yes = cmd;
+
+	if	(yes[0] == 1)	{return __login();}
+	else if	(yes[0] == 2)	{return __account(yes[1]);}
+	else if	(yes[0] == 3)	{return __logout();}
+	else if	(yes[0] == 4)	{return -1;}
+	else if	(yes[0] == 5)	{return __push_market_order(yes+1);}
+	else if	(yes[0] == 6)	{return __commit_order(yes+1);}
+	else if	(yes[0] == 7)	{return __push_order(yes+1);}
+
+	else 			{_D(("Unknow command type\n")); return 1;}
+
+}
+
+char  RunCommand(int __cs);
+char  RunCommand(int __cs)
+{
+	/*
+	   recv to get one byte (size)
+	   recv to get data of size bytes into command buffer
+	   now , I get full command data
+	   run parse;
+	   loop
+	 */
+
+	char __size;
+	int _r;
+	char __cmdb[1024];
+
+	_r = recv (__cs,&__size,1,0);
+	if(_r != 1)
+		return -1;
+
+	_D(("command.c:size is %d\n",__size));
+
+	_r = recv(__cs,__cmdb,__size-1,0);
+	if(_r != __size-1)
+		return -1;
+
+	_D(("command.c:read command %d bytes\n",_r));
+
+	return __run_command_parse(__cs,__cmdb,__size-1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 char __parse_order_market(char* yes);
 char __parse_order_market(char* yes)
 {
@@ -77,85 +154,97 @@ char __parse_order_price(char* yes)
 	return 1;
 }
 
-char  __run_command_parse(int socket,void* cmd,int cmdsize);
-char  __run_command_parse(int socket,void* cmd,int cmdsize)
+char __login(void)
 {
-
-	char* yes = cmd;
-	int _r;
-
-	_r = 1;
-	switch (yes[0]) {
-		case 1:
-			printf("%s Login ...\n",LoginId);
-			_r = OL_LoginServer(LoginId,Password);
-			printf("return %d\n",_r);
-			_r = 1;
-			break;
-
-		case 2:
-			printf("Select a account\n");
-			OL_SetTradeAccount(yes[1]);
-			break;
-
-		case 3:
-			printf("Logout\n");
-			OL_Bye();
-			break;
-
-		case 4:
-			printf("exit\n");
-			_r = -1;
-			break;
-
-		case 5:
-			printf("push market order\n");
-			_r = __parse_order_market(yes+1);
-			break;
-
-		case 6:
-			printf("commit price order\n");
-			_r = __parse_order_price(yes+1);
-			break;
-
-		case 7:
-			printf("push order\n");
-			break;
-
-		default:
-			printf("command.c: unknow , unsupport , unimplement command\n");
-			break;
+	char _r;
+	_L(("info:\tstart login command\n"));
+	_r = OL_LoginServer(LoginId,Password);
+	_D(("login process return %d\n",_r));
+	
+	if( _r == 1)
+	{
+		_L(("info:\tlogin process OK\n"));
 	}
+	else
+	{
+		_L(("info:\tlogin process fail\n"));
+	}
+	
 	return _r;
 }
 
-char  RunCommand(int __cs);
-char  RunCommand(int __cs)
+char __logout(void)
 {
-	/*
-	   recv to get one byte (size)
-	   recv to get data of size bytes into command buffer
-	   now , I get full command data
-	   run parse;
-	   loop
-	 */
 
-	char size;
-	int _r;
-	char __cmdb[1024];
+	_L(("info:\tstart logout command\n"));	
+	OL_Bye();
 
-	_r = recv (__cs,&size,1,0);
-	if(_r != 1)
-		return -1;
-
-	printf("command.c:size is %d\n",size);
-
-	_r = recv(__cs,__cmdb,size-1,0);
-	if(_r != size-1)
-		return -1;
-
-	printf("command.c:read command %d bytes\n",_r);
-
-	return __run_command_parse(__cs,__cmdb,size-1);
+	_L(("info:\tlogout process OK\n"));
+	return 1;
 }
 
+char __account(char index)
+{
+	char _r;
+	
+	_L(("info:\tstart select account command\n"));
+	_D(("account is %d\n",index));
+	_r = OL_SetTradeAccount(index);
+	_D(("OL_SetTradeAccount return is %d\n",_r));
+
+	if( _r == 1)
+	{
+		_L(("info:\tselect account process OK\n"));
+	}
+	else
+	{
+		_L(("info:\tselect account process fail\n"));
+	}
+	
+	return _r;
+}
+
+char __push_market_order(char* cmdbuf)
+{
+	char _r;
+
+	_L(("info:\tpush Market order\n"));	
+	_r = __parse_order_market(cmdbuf);
+	_D(("return is %d\n",_r));
+	
+
+	if( _r == 1)
+	{
+		_L(("info:\tpush Market order OK\n"));
+	}
+	else
+	{
+		_L(("info:\tpush Market order fail\n"));
+	}
+	
+	return _r;
+}
+
+char __commit_order(char* cmdbuf)
+{
+	char _r;
+	_L(("info:\tstart commit a order\n"));
+	_r = __parse_order_price(cmdbuf);
+	_D(("return is %d\n",_r));
+
+	if( _r == 1)
+	{
+		_L(("info:\tcommit order OK\n"));
+	}
+	else
+	{
+		_L(("info:\tcommit order fail\n"));
+	}
+
+	return _r;
+}
+
+char __push_order(char* cmdbuf)
+{
+	return 1;
+}
--- order_server/main.c
+++ order_server/main.c
@@ -4,12 +4,26 @@
 #include <winsock2.h>
 #include "orderapi.h"
 
-static int __defaultport=1677;
+#ifdef DEBUG
+#define _D(x)	printf x
+#else
+#define _D(x)
+#endif
+
+#define	_L(x)	printf x
+
+
 
 extern char  RunCommand(int __cs); //declare on command.c
 
+extern char LoginId[32];	//export to command.c
+extern char Password[128];	//export to command.c
+extern SOCKET ClientSocket;	//export to command.c
 char LoginId[32];
 char Password[128];
+SOCKET ClientSocket=-1;
+
+static int __defaultport=1677;
 
 int main(int argc,char** argv);
 int main(int argc,char** argv)
@@ -23,24 +37,24 @@ int main(int argc,char** argv)
 		return 0;
 	}
 
-	if(argc == 3)
-	{
-		printf("usage: order_server LoginID Password [port_number]\n"
-				"\t port_number is server bind port,client will connect with this port"
-				"\n default port is 1677\n");
-	}
-	else
-		__defaultport=atoi(argv[3]);
-
+	_L(("info:\tOrder server start\n"));
 	strcpy(LoginId, argv[1]);
 	strcpy(Password,argv[2]);
+	if(argc > 3)
+	{
+		__defaultport=atoi(argv[3]);
+	}
 
+	_L(("info:\tLogin ID %s\n",LoginId));
+	_L(("info:\tPassword %s\n",Password));
+	_L(("info:\tconnect port %d\n",__defaultport));
 
 	//init Windows socket
+	_L(("info:\tcreate network interface\n"));
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
 	{
-		printf("WSAStartup() failed\n");
+		_D(("WSAStartup() failed\n"));
 		return 0;
 	}
 
@@ -49,7 +63,7 @@ int main(int argc,char** argv)
 	__ls=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 	if (__ls==INVALID_SOCKET)
 	{
-		printf("socket() Failed\n");
+		_D(("socket() Failed\n"));
 		WSACleanup();
 		return 0;
 	}
@@ -60,36 +74,39 @@ int main(int argc,char** argv)
 	sin.sin_addr.S_un.S_addr=htonl(INADDR_ANY);
 	if (bind(__ls,(struct sockaddr*)&sin,sizeof(sin))==SOCKET_ERROR)
 	{
-		printf("bind() Failed\n");
+		_D(("bind() Failed\n"));
 		WSACleanup();
 		return 0;
 	}
 
 	if (listen(__ls,1)==SOCKET_ERROR)
 	{
-		printf("listen() Failed\n");
+		_D(("listen() Failed\n"));
 		WSACleanup();
 		return 0;
 	}
 
-	SOCKET __cs; //Client Socket
-	__cs=accept(__ls,NULL,NULL);
-	if (__cs==INVALID_SOCKET)
+
+	_L(("info:\twait connect\n"));
+	ClientSocket=accept(__ls,NULL,NULL);
+	if (ClientSocket==INVALID_SOCKET)
 	{
-		printf("accept() fail\n");
+		_D(("accept() fail\n"));
 		closesocket(__ls);
 		WSACleanup();
 		return 0;
 	}
 
-	while(RunCommand(__cs) == 1)
+	_L(("info:\tconnect create.ready to read command\n"));
+	while(RunCommand(ClientSocket) == 1)
 	{
-		printf("Finish one command\n");
+		_D(("Finish one command\n"));
 	}
 
-	closesocket(__cs);
+	closesocket(ClientSocket);
 	closesocket(__ls);
 	WSACleanup();
+	_L(("info:\tTerminate\n"));
 	return 0;
 }
 
