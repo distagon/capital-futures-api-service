7f896714b72a293b210d306bd3dac54fc80d20b1
leolarrel <leolarrel@localhost>
2012-12-19 01:32:24 +0800

    order_server : improving test tool

--- order_server/test_command.py
+++ order_server/test_command.py
@@ -1,13 +1,30 @@
 import socket
 import sys
-import struct
 
-A=struct.pack('2b',2,1);
 ssoo = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-ssoo.connect(('172.17.4.129',1677));
+ssoo.connect(('127.0.0.1',1677));
 
-for a in xrange(1,1000) :
-    ssoo.send(A);
+#login
+#A=struct.pack('2b',2,1);
+ssoo.send(b'\x02\x01');
+
+#select account
+#A=struct.pack('3b',3,2,1);
+ssoo.send(b'\x03\x02\x01');
+
+#order market
+#a=b'\x05MTX12\0\x01\x01\x02';
+#b=len(a);
+#b+a;
+#ssoo.send(b);
+
+#logout
+#A=struct.pack('2b',2,3);
+ssoo.send(b'\x02\x03');
+
+#exit
+#A=struct.pack('2b',2,4);
+ssoo.send(b'\x02\x04');
 
 ssoo.close();
 
